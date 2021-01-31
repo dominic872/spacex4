a15d829595d067e965a45daf73d11659c88b846b
dominic872 <77916035+dominic872@users.noreply.github.com>
2021-01-31 17:44:08 +0530

    First commit

---
+++ config/paths.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+module.exports = {
+  // Source files
+  src: path.resolve(__dirname, '../src'),
+  template: path.resolve(__dirname, '../src/template'),
+  data: path.resolve(__dirname, '../src/data'),
+
+  // Production build files
+  build: path.resolve(__dirname, '../dist'),
+
+  // Static files that get copied to build folder
+  public: path.resolve(__dirname, '../public'),
+}
